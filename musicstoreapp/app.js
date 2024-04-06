4106ea32b1b511f5c1839146b9e14d7cc6bd290f
Ángel Moltó <uo287747@uniovi.es>
2024-04-06 20:49:43 +0200

    SDI-2324-312-11.3-SW Cliente JQuery + AJAX.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -6,10 +6,19 @@ var logger = require('morgan');
 
 var app = express();
 
-//JSON Web Token
+// Cabeceras Access-Control-Allow-*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  // Debemos especificar todas las headers que se aceptan. Content-Type , token
+  next();
+});
+
+// JSON Web Token
 let jwt = require('jsonwebtoken');
 app.set('jwt', jwt);
-
 
 // Express Session
 let expressSession = require('express-session');
